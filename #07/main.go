35606e3e6cf795bbfd553ba93e82c00c99fad3ac
CharlieYu4994 <charlieyu4994@outlook.com>
2021-01-23 18:01:33 +0800

    学习ing

--- #07/main.go
+++ #07/main.go
@@ -1,7 +1,45 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
+
+var (
+	coins = 50
+	users = []string{
+		"Matthew", "Sarah", "Augustus", "Heidi", "Emilie", "Peter", "Giana", "Adriano", "Aaron", "Elizabeth",
+	}
+	distribution = make(map[string]int, len(users))
+)
+
+func statName(name string) (sum int) {
+	tmp := []byte(name)
+	for _, litter := range tmp {
+		if litter == 'e' {
+			sum++
+		} else if litter == 'i' {
+			sum += 2
+		} else if litter == 'o' {
+			sum += 3
+		} else if litter == 'u' {
+			sum += 4
+		}
+	}
+	return
+}
+
+func dispatchCoin(num int, people []string) (sum int) {
+	for _, name := range people {
+		num := statName(strings.ToLower(name))
+		distribution[name] = num
+		sum += num
+	}
+	return
+}
 
 func main() {
-	fmt.Println("hello world")
+	left := dispatchCoin(coins, users)
+	fmt.Println("剩下：", left)
+	fmt.Println(distribution)
 }
